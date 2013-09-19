dd995be7806d61a1f7378b1400eafc5d5b96bdf6
Patrick Hyatt <patrick.hyatt@gmail.com>
2013-09-19 09:50:19 -0700

    Corrected missing projects. Added explicit example.

--- XMPP_bot/Program.cs
+++ XMPP_bot/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.Configuration;
-using System.Reflection;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +11,7 @@ using agsXMPP;
 using agsXMPP.protocol.client;
 using agsXMPP.protocol.iq.roster;
 using agsXMPP.protocol.x.muc;
-
 using XmppBot.Common;
-using System.Timers;
 
 namespace XMPP_bot
 {
@@ -23,10 +20,15 @@ namespace XMPP_bot
         private static DirectoryCatalog _catalog = null;
         private static XmppClientConnection _client = null;
         private static Dictionary<string, string> _roster = new Dictionary<string, string>(20);
-        private static Dictionary<string, string> _tempRoster = new Dictionary<string, string>(20);
 
         static void Main(string[] args)
         {
+            string pluginsDirectory = Environment.CurrentDirectory + "\\plugins\\";
+            if (!Directory.Exists(pluginsDirectory))
+            {
+                Directory.CreateDirectory(pluginsDirectory);
+            }
+
             _catalog = new DirectoryCatalog(Environment.CurrentDirectory + "\\plugins\\");
             _catalog.Changed += new EventHandler<ComposablePartCatalogChangeEventArgs>(_catalog_Changed);
             LoadPlugins();
@@ -38,6 +40,7 @@ namespace XMPP_bot
 
             _client.OnLogin += new ObjectHandler(xmpp_OnLogin);
             _client.OnMessage += new MessageHandler(xmpp_OnMessage);
+            _client.OnError += new ErrorHandler(_client_OnError);
 
             Console.WriteLine("Connecting...");
             _client.Resource = ConfigurationManager.AppSettings["Resource"];
@@ -46,12 +49,16 @@ namespace XMPP_bot
 
             _client.OnRosterStart += new ObjectHandler(_client_OnRosterStart);
             _client.OnRosterItem += new XmppClientConnection.RosterHandler(_client_OnRosterItem);
-            _client.OnRosterEnd += new ObjectHandler(_client_OnRosterEnd);
 
             while (true)
             {
                 System.Threading.Thread.Sleep(200);
             }
+        }
+
+        static void _client_OnError(object sender, Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex);
         }
 
         static void _catalog_Changed(object sender, ComposablePartCatalogChangeEventArgs e)
@@ -61,18 +68,13 @@ namespace XMPP_bot
 
         static void _client_OnRosterStart(object sender)
         {
-            _tempRoster = new Dictionary<string, string>(20);
-        }
-
-        static void _client_OnRosterEnd(object sender)
-        {
-            _roster = _tempRoster;
+            _roster = new Dictionary<string, string>(20);
         }
 
         static void _client_OnRosterItem(object sender, RosterItem item)
         {
-            if (!_tempRoster.ContainsKey(item.Jid.User))
-                _tempRoster.Add(item.Jid.User, item.Name);
+            if (!_roster.ContainsKey(item.Jid.User))
+                _roster.Add(item.Jid.User, item.Name);
         }
 
         static void xmpp_OnLogin(object sender)
@@ -83,8 +85,7 @@ namespace XMPP_bot
 
             foreach (string room in rooms)
             {
-                Jid jid = new Jid(room + "@" + "conf.hipchat.com");
-                //mucManager.AcceptDefaultConfiguration(jid);
+                Jid jid = new Jid(room + "@" + ConfigurationManager.AppSettings["ConferenceServer"]);
                 mucManager.JoinRoom(jid, ConfigurationManager.AppSettings["RoomNick"]);
             }
         }
@@ -117,17 +118,17 @@ namespace XMPP_bot
                 switch (line.Command)
                 {
                     case "close":
-                        SendMessage(msg.From.Bare, "I'm a quitter...", msg.Type);
+                        SendMessage(msg.From, "I'm a quitter...", msg.Type);
                         Environment.Exit(1);
                         return;
 
                     case "reload":
-                        SendMessage(msg.From.Bare, LoadPlugins(), msg.Type);
+                        SendMessage(msg.From, LoadPlugins(), msg.Type);
                         break;
 
                     default:
                         Parallel.ForEach<IXmppBotPlugin>(Plugins,
-                            plugin => SendMessage(msg.From.Bare, plugin.Evaluate(line), msg.Type)
+                            plugin => SendMessage(msg.From, plugin.Evaluate(line), msg.Type)
                             );
 
                         break;
---
+++ XmppBot.Plugins/Example.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+using XmppBot.Common;
+
+namespace XmppBot.Plugins
+{
+    [Export(typeof(IXmppBotPlugin))]
+    public class Example : IXmppBotPlugin
+    {
+        public string Evaluate(ParsedLine line)
+        {
+            if (!line.IsCommand) return string.Empty;
+
+            switch (line.Command.ToLower())
+            {
+                case "smack":
+                    return String.Format("{0} smacks {1} around with a large trout", line.User, line.Args.FirstOrDefault() ?? "your mom");
+
+                case "hug":
+                    return String.Format("{0} hugs {1}", line.User, line.Args.FirstOrDefault() ?? "themself");
+
+                default:
+                    return null;
+            }
+        }
+
+        public string Name
+        {
+            get { return "User Actions"; }
+        }
+    }
+}
